4ea112577a28f33e9f0e48e94702ece8a8b6dbcc
Randy Sugianto <yukuku@gmail.com>
2010-03-19 14:15:23 +0800

    Use the latest gofmt that removes the semicolons

--- src/pkg/dump/dump_test.go
+++ src/pkg/dump/dump_test.go
@@ -1,28 +1,28 @@
 package dump_test
 
 import (
-	. "dump";
-	"testing";
-	"go/parser";
-	"go/token";
-	"fmt";
+	. "dump"
+	"testing"
+	"go/parser"
+	"go/token"
+	"fmt"
 )
 
 
 var emptyString = ""
 
 type S struct {
-	A int;
-	B int;
+	A int
+	B int
 }
 
 type T struct {
-	S;
-	C int;
+	S
+	C int
 }
 
 type Circular struct {
-	c *Circular;
+	c *Circular
 }
 
 func TestDump(t *testing.T) {
@@ -30,25 +30,25 @@ func TestDump(t *testing.T) {
 
 	// func ParseFile(filename string, src interface{}, scope *ast.Scope, mode uint) (*ast.File, os.Error)
 
-	file, e := parser.ParseFile("dump_test.go", nil, nil, parser.ParseComments);
+	file, e := parser.ParseFile("dump_test.go", nil, nil, parser.ParseComments)
 	if e != nil {
 		fmt.Println("error", e)
 	} else {
 		//fmt.Printf("%#v\n", file);
-		Dump(file);
-		Dump(map[string]int{"satu": 1, "dua": 2});
-		Dump([]int{1, 2, 3});
-		Dump([3]int{1, 2, 3});
-		Dump(&[][]int{[]int{1, 2, 3}, []int{1, 2, 3}, []int{1, 2, 3}});
-		Dump(&emptyString);
-		Dump(T{S{1, 2}, 3});
-		Dump(token.STRING);
+		Dump(file)
+		Dump(map[string]int{"satu": 1, "dua": 2})
+		Dump([]int{1, 2, 3})
+		Dump([3]int{1, 2, 3})
+		Dump(&[][]int{[]int{1, 2, 3}, []int{1, 2, 3}, []int{1, 2, 3}})
+		Dump(&emptyString)
+		Dump(T{S{1, 2}, 3})
+		Dump(token.STRING)
 
-		bulet := make([]Circular, 3);
-		bulet[0].c = &bulet[1];
-		bulet[1].c = &bulet[2];
-		bulet[2].c = &bulet[0];
+		bulet := make([]Circular, 3)
+		bulet[0].c = &bulet[1]
+		bulet[1].c = &bulet[2]
+		bulet[2].c = &bulet[0]
 
-		Dump(struct{ a []Circular }{bulet});
+		Dump(struct{ a []Circular }{bulet})
 	}
 }
